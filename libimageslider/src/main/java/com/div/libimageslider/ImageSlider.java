0ad0e169be20eb457b2366cc9893e96fbcbe50ab
divyanshu1610 <divyanshu1610@gmail.com>
2019-05-29 19:26:38 +0530

    basic-animations

--- libimageslider/src/main/java/com/div/libimageslider/ImageSlider.java
+++ libimageslider/src/main/java/com/div/libimageslider/ImageSlider.java
@@ -1,17 +1,17 @@
 package com.div.libimageslider;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -21,10 +21,7 @@ import org.jetbrains.annotations.Nullable;
 public class ImageSlider extends View {
 
 
-
-    GestureDetector gestureDetector;
-
-    int mRight;
+    int midPoint;
     private static final int FIRST_COLOR_DEF = Color.GREEN;
     private static final int SECOND_COLOR_DEF = Color.RED;
     private static final String TAG ="ImageSlider";
@@ -78,7 +75,6 @@ public class ImageSlider extends View {
 
     private void initImageSlider(@Nullable  AttributeSet set) {
 
-        gestureDetector = new GestureDetector(getContext(), new GestureListener());
 
         if (set == null)
             return;
@@ -117,7 +113,7 @@ public class ImageSlider extends View {
         ta.recycle();
 
 
-        mRight = getDefaultWidth()/2;
+        midPoint = getDefaultWidth()/2;
 
     }
 
@@ -129,12 +125,15 @@ public class ImageSlider extends View {
         //Initialize RectOne
         mRectOne.left = 0;
         mRectOne.top = 0;
-        mRectOne.right = mRight;
+        //Right will change only
+        mRectOne.right = midPoint;
         mRectOne.bottom = mSliderHeight;
 
 
         //Initialize RectTwo
-        mRectTwo.left = mRight;
+
+        //Left will change only
+        mRectTwo.left = midPoint;
         mRectTwo.top = 0;
         mRectTwo.right = getDefaultWidth();
         mRectTwo.bottom = mSliderHeight;
@@ -160,43 +159,106 @@ public class ImageSlider extends View {
     }
 
 
-
-
-    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
-        // event when double tap occurs
-
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
-            Log.d(TAG, "onFling: X="+velocityX+" Y:"+velocityY);
-
-            Log.d(TAG, "onFling: e1="+e1);
-            Log.d(TAG, "onFling: e2="+e2);
-            return super.onFling(e1, e2, velocityX, velocityY);
-        }
-    }
-
+    int posX,posY;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
 
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                posX = (int)event.getX();
+                posY = (int)event.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                mRight = mRight+100;
+
+                Log.d(TAG, "onTouchEvent: X: "+event.getX());
+
+                if(posX < event.getX())
+                    swipeRight();
+                else if(posX > event.getX())
+                    swipeLeft();
                 break;
             case MotionEvent.ACTION_UP:
+                decideIt();
                 break;
             default:
                 return false;
         }
 
-        gestureDetector.onTouchEvent(event);
-       postInvalidate();
+        postInvalidate();
         return true;
+    }
+
+
+
+    private void swipeRight(){
+        midPoint+=10;
+        //postInvalidate();
+    }
+
+
+    private void swipeLeft(){
+        midPoint-=10;
+        //postInvalidate();
+    }
+
+
+
+    private void animate(int dest){
+
+        ValueAnimator animation = ValueAnimator.ofInt(midPoint,dest);
+        animation.setDuration(300);
+        animation.start();
+
+        animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator updatedAnimation) {
+                // You can use the animated value in a property that uses the
+                // same type as the animation. In this case, you can use the
+                // float value in the translationX property.
+                int animatedValue =(int)updatedAnimation.getAnimatedValue();
+                midPoint = animatedValue;
+               // Log.d(TAG, "onAnimationUpdate: "+midPoint);
+                postInvalidate();
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+
+    private void decideIt(){
+
+
+        int temp;
+
+        if(midPoint > getDefaultWidth()/2){
+
+            temp = (3*getDefaultWidth())/4;
+            if(midPoint > temp) {
+                //animate to FULL
+               animate(getDefaultWidth());
+            }else{
+                //animate to MID
+                animate(getDefaultWidth()/2);
+            }
+        }else{
+            temp = getDefaultWidth()/4;
+            if(midPoint < temp) {
+                //animate to FULL
+                animate(0);
+            }else{
+                //animate to MID
+                animate(getDefaultWidth()/2);
+            }
+        }
     }
 
 }
